c4c7e7ea343a2b8b72e285569decd4522188eae1
manuelareis <manu.mreis@hotmail.com>
2016-07-13 17:38:17 -0300

    Update Foods.cs
    add dificulty

--- Foods.cs
+++ Foods.cs
@@ -1,12 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Foods : MonoBehaviour
 {
-    public float timeFade = 1f;
-    public float fadespeed = 1f;
-    public float fade;
-    public float speed = 5f;
+    float timeFade = 1f;
+    float fadespeed = 1f;
+    float fade;
+    [SerializeField] float speed = 5f;
     
     //cam sizes
     Camera cam;
@@ -28,13 +28,13 @@ public class Foods : MonoBehaviour
         camHeight =  cam.orthographicSize;
         camWidth = camHeight * cam.aspect;
 
+        speed = 5f;
         //get render component to change alpha
         srFood = GetComponent<SpriteRenderer>();      
         startTime = Time.time;
-            
+                
     }
     void Update () {
-
         if (move){
             transform.position -= new Vector3(0f, 1f * speed * Time.deltaTime, 0f);
             transform.Rotate(Vector3.back * 50 * Time.deltaTime);
@@ -54,11 +54,14 @@ public class Foods : MonoBehaviour
         }
     }
 
-    void LosePoint()
-    {
+    void LosePoint(){
             GM.gmInstance.setScore(0, -1);
             lostFood = true;
     }
 
+    void setSpeed(float vel)
+    {
+        speed = vel;
+    }
 
 }
